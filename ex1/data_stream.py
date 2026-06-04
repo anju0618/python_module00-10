93a768deb336512f4386b5f1df30d9fe1b8fed96
Anjou Makino <amakino@c4r1s6.42tokyo.jp>
2026-06-04 18:27:08 +0900

    anjou

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
 import typing
 import abc
 
 
+class DataStream:
+    def __init__(self) -> None:
+        self._processors: list[DataProcessor] = []
+
+    def register_processor(self, proc: DataProcessor) -> None:
+        self._processors.append(proc)
+
+    def process_stream(self, stream: list[typing.Any]) -> None:
+        for element in stream:
+            found = False
+            for proc in self._processors:
+                if proc.validate(element):
+                    proc.ingest(element)
+                    found = True
+                    break
+            if not found:
+                print(f"Can't process element in stream: {element}")
+
+    def print_processors_stats(self) -> None:
+        print("== DataStream statistics ==")
+        if not self._processors:
+            print("No processor found, no data")
+            return
+
+        for proc in self._processors:
+            name = proc.__class__.__name__.replace("Processor", " Processor")
+            total = proc.get_total_processed()
+            remaining = proc.get_remaining_count()
+            print(f"{name}: total {total} items processed,"
+                  f" remaining {remaining} on processor")
+
+
+"""
+ex0の部品>>>
+"""
+
+
 class DataProcessor(abc.ABC):
+
     def __init__(self) -> None:
         self._storage: list[tuple[int, str]] = []
         self._rank_counter: int = 0
@@ -21,6 +60,13 @@ class DataProcessor(abc.ABC):
         if not self._storage:
             return (-1, "")
         return self._storage.pop(0)
+
+    # add def
+    def get_total_processed(self) -> int:
+        return self._rank_counter
+
+    def get_remaining_count(self) -> int:
+        return len(self._storage)
 
 
 class NumericProcessor(DataProcessor):
@@ -104,51 +150,64 @@ class LogProcessor(DataProcessor):
                 self._rank_counter += 1
 
 
+"""
+ex0の部品<<<
+"""
+
+
 def main() -> None:
-    print("=== Code Nexus Data Processor ===")
+    print("=== Code Nexus Data Stream ===")
 
-    print("\nTesting Numeric Processor.")
+    print("\nInitialize Data Stream..")
+    stream_sys = DataStream()
+    stream_sys.print_processors_stats()
+
     num_proc = NumericProcessor()
-    print(f"Trying to validate input '42': {num_proc.validate(42)}")
-    print(f"Trying to validate input 'Hello': {num_proc.validate('Hello')}")
-
-    print("Test invalid ingestion of string 'foo' without prior validation:")
-    try:
-        num_proc.ingest("foo")  # type:ignore
-    except ValueError as e:
-        print(f"Got exception: {e}")
-
-    print("Processing data: [1, 2, 3, 4, 5]")
-    num_proc.ingest([1, 2, 3, 4, 5])
-    print("Extracting 3 values...")
-    for i in range(3):
-        rank, val = num_proc.output()
-        print(f"Numeric value {i}: ", end="")
-        print(val)
-
-    print("\nTesting Text Processor.")
     text_proc = TextProcessor()
-    print(f"Trying to validate input '42': {text_proc.validate(42)}")
-    print("Processing data: ['Hello', 'Nexus', 'World']")
-    text_proc.ingest(['Hello', 'Nexus', 'World'])
-    print("Extracting 1 value...")
-    rank, val = text_proc.output()
-    print(f"Text value 0: {val}")
-
-    print("\nTesting Log Processor.")
     log_proc = LogProcessor()
-    print(f"Trying to validate input 'Hello': {log_proc.validate('Hello')}")
 
-    log_data = [
-        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
-        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!!'}
+    print("\nRegistering Numeric Processor")
+    stream_sys.register_processor(num_proc)
+
+    batch = [
+        'Hello world',
+        [3.14, 1, -2.71],
+        [
+            {
+                'log_level': 'WARNING',
+                'log_message': 'Telnet access! Use ssh instead'
+            },
+            {
+                'log_level': 'INFO',
+                'log_message': 'User wil is connected'
+            }
+        ],
+        42,
+        ['Hi', 'five']
     ]
-    print(f"Processing data: {log_data}")
-    log_proc.ingest(log_data)
-    print("Extracting 2 values...")
-    for i in range(2):
-        rank, val = log_proc.output()
-        print(f"Log entry {i}: {val}")
+
+    print("\nSend first batch of data on stream:")
+    stream_sys.process_stream(batch)
+    stream_sys.print_processors_stats()
+
+    print("\nRegistering other data processors")
+    stream_sys.register_processor(text_proc)
+    stream_sys.register_processor(log_proc)
+
+    print("Send the same batch again")
+    stream_sys.process_stream(batch)
+    stream_sys.print_processors_stats()
+
+    print("\nConsume some elements from the"
+          "data processors: Numeric 3, Text 2, Log 1")
+    for _ in range(3):
+        num_proc.output()
+    for _ in range(2):
+        text_proc.output()
+    for _ in range(1):
+        log_proc.output()
+
+    stream_sys.print_processors_stats()
 
 
 if __name__ == "__main__":
